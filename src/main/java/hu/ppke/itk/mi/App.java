cf72bf7937dda0437d4878e214913b05c486b2c5
Zoltan Lajtavari <lajtavariz@gmail.com>
2018-03-24 19:11:01 +0100

    Initial commit

---
+++ src/main/java/hu/ppke/itk/mi/App.java
@@ -0,0 +1,13 @@
+package hu.ppke.itk.mi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
